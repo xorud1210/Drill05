432db8894a73ae59b7e5a8d2a5b051241862f92e
TIP209 <143979587+xorud1210@users.noreply.github.com>
2023-09-27 18:52:57 +0900

    수업 끝

--- line.py
+++ line.py
@@ -52,13 +52,33 @@ def draw_point(p):
 
 def draw_line(p1, p2):
     # fill here
-    pass
+    draw_big_point(p1)
+    draw_big_point(p2)
+    x1, y1 = p1[0], p1[1] #(-100, -100)
+    x2, y2 = p2[0], p2[1] #(300, 150)
+
+    for i in range(0, 100, 5):
+        t = i / 100
+
+        x = (1-t)*x1 + t*x2 # 1-t:t의 비율로 x1, x2를 섞는다or더한다
+        y = (1-t)*y1 + t*y2
+
+        draw_point((x, y))
+
+    draw_point(p2)
 
 
 prepare_turtle_canvas()
 
-
 # fill here
+# points = [(-300, 200), (400, 350), (300, -300), (-200,-200)]
+points = [(random.randint(-300, 300), random.randint(-300, 300)) for i in range(10)]
 
+for p in points:
+    draw_big_point(p)
+
+for i in range(0,len(points) - 1):
+    draw_line(points[i], points[i + 1])
+draw_line(points[-1],points[0])
 
 turtle.done()
