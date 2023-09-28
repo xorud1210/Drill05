65bb5b78160540348ca0fc2020d587289a996e2d
xorud1210 <rhxo1210@naver.com>
2023-09-28 16:29:48 +0900

    손 순간이동 함수 작동 확인

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -12,24 +12,18 @@ hand = load_image('hand_arrow.png')
 
 def handle_events():
     global running
-    global x_cha, y_cha
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        # elif event.type == SDL_MOUSEMOTION:
-        #     x_cha, y_cha = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
-    pass
 
 def teleport_hand():
-    print('손가락 이동')
-    pass
-    #global x_hand, y_hand
+    global x_hand, y_hand
+    x_hand, y_hand = random.randint(0,TUK_WIDTH),random.randint(0,TUK_HEIGHT)
 
 def move_character():
-    print('캐릭터 이동')
     pass
 
 
@@ -41,15 +35,13 @@ frame = 0
 hide_cursor()
 
 while running:
+    teleport_hand()
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    hand.draw(x_hand,y_hand)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x_cha, y_cha)
+    hand.draw(x_hand, y_hand)
     update_canvas()
-    move_character()
-    teleport_hand()
-    frame = (frame + 1) % 8
-
+    delay(0.1)
+    #move_character()
     handle_events()
 
 close_canvas()
