a5f280376af458658a796f3c1846c5ff3b4bebc0
xorud1210 <rhxo1210@naver.com>
2023-09-28 16:14:49 +0900

    뼈대 만들기

--- move_character_with_mouse.py
+++ move_character_with_mouse.py
@@ -1,3 +1,5 @@
+import random
+
 from pico2d import *
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
@@ -5,33 +7,47 @@ open_canvas(TUK_WIDTH, TUK_HEIGHT)
 
 TUK_ground = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+hand = load_image('hand_arrow.png')
 
 
 def handle_events():
     global running
-    global x, y
+    global x_cha, y_cha
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
+        # elif event.type == SDL_MOUSEMOTION:
+        #     x_cha, y_cha = event.x, TUK_HEIGHT - 1 - event.y
         elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
             running = False
     pass
 
+def teleport_hand():
+    print('손가락 이동')
+    pass
+    #global x_hand, y_hand
+
+def move_character():
+    print('캐릭터 이동')
+    pass
 
 
 running = True
-x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
+x_cha, y_cha = TUK_WIDTH // 2, TUK_HEIGHT // 2
+x_hand, y_hand = random.randint(0,TUK_WIDTH),random.randint(0,TUK_HEIGHT)
+
 frame = 0
 hide_cursor()
 
 while running:
     clear_canvas()
     TUK_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
-    character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
+    hand.draw(x_hand,y_hand)
+    character.clip_draw(frame * 100, 100 * 1, 100, 100, x_cha, y_cha)
     update_canvas()
+    move_character()
+    teleport_hand()
     frame = (frame + 1) % 8
 
     handle_events()
